c913d7454e7a21906f5ed638b349a15b7b2054ba
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-23 21:43:12 +1100

    cleaned up code, removed unneccesary console.logs, ready to be put up on heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,6 @@
-var bodyParser = require('body-parser')
 const { uuid } = require('uuidv4');
 const fs = require("fs");
 const util = require("util");
-const { parse } = require('path');
-// const note = require("../db/db.json");
 
 const writeFileAsync = util.promisify(fs.writeFile)
 const readFileAsync = util.promisify(fs.readFile)
@@ -13,18 +10,12 @@ const notes = "./db/db.json"
 
 function apiRoutes(app) {
 
-    // app.use(bodyParser.urlencoded({
-    //   extended: true
-    // }));
 
     app.get("/api/notes", async function (req, res) {
         try {
             const note = await readFileAsync(notes, "utf-8")
             const parseNote = JSON.parse(note)
-
             res.json(parseNote)
-
-
         } catch (err) {
             if (err) {
                 console.log(err)
@@ -34,15 +25,11 @@ function apiRoutes(app) {
 
     app.post("/api/notes", async function (req, res) {
         try {
-
             const note = await readFileAsync(notes, "utf-8")
             const id = uuid();
             req.body.id = id
             const parseNote = JSON.parse(note)
-            console.log("hey")
             parseNote.push(req.body)
-
-
 
             await writeFileAsync(notes, JSON.stringify(parseNote, null, 2))
             res.json(req.body)
@@ -54,14 +41,11 @@ function apiRoutes(app) {
         }
     })
 
-
     app.delete('/api/notes/:id', async function (req, res) {
         try {
             const note = await readFileAsync(notes, "utf-8")
             const id = req.params.id;
-
             const parseNote = JSON.parse(note)
-
             parseNote.forEach(value => {
                 if (value.id === id) {
                     parseNote.splice(parseNote.indexOf(value), 1);
@@ -70,37 +54,13 @@ function apiRoutes(app) {
                 }
             });
 
-
             await writeFileAsync(notes, JSON.stringify(parseNote, null, 2))
             res.json(req.body)
-
 
         } catch (err) {
             throw err;
         }
-
-        // allNotes.filter(function (note) {
-        //   return note.id !== req.params.id;
-        // });
-        // const note = await readFileAsync(notes, "utf-8")
-
-
-
-
-        //  console.log(JSON.stringify(data));
-        //  res.status(200);
-
-        //   return res.send("Removed");
     });
-
 }
-
-
-
-
-
 module.exports = apiRoutes;
 
-// let rawdata = fs.readFileSync('student.json');
-// let student = JSON.parse(rawdata);
-// console.log(student);
